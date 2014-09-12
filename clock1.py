198001d9a404e3da20c17e3f62fd3c0671b3c583
Philippe Chretien <philippe.chretien@gmail.com>
2014-09-12 19:00:43 -0400

    Framework for the clock1 driver script

--- clock1.py
+++ clock1.py
@@ -1,1 +1,61 @@
-print('Hello World')
+#!/usr/bin/python
+import time
+import sys
+import atexit
+from threading import Timer
+import RPi.GPIO as GPIO
+
+# Display detailed information when set to true
+verbose = False
+
+# tic every second
+sleepDelay = 61
+
+# Display a message to the console if the verbose mode is enabled
+def printInfo(msg):
+    if verbose:
+        print msg
+
+# Print an error message
+def printError(error):
+    print(error)
+
+# Process the command line arguments if any
+def processArgv():
+    global verbose
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i] == '-i':
+            print('-i: Ignoring current mentions')
+            ignoreCurrentTweets()
+
+        if sys.argv[i] == '-v':
+            print('-v: Activating verbose mode')
+            verbose = True
+
+def clearGPIO():
+    GPIO.cleanup()
+    print('Bye!')
+
+atexit.register(clearGPIO)
+
+def initGPIO():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+
+#
+# Main
+#
+
+# Init the IOs
+initGPIO()
+
+# Read the command line arguments
+processArgv()
+
+printInfo('Starting clock1 ...')
+
+while True:
+    print('tic')
+
+    time.sleep(sleepDelay)
